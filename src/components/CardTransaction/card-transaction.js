04390fb6a232f0d172bfebb78a031963114d2345
emachipana <emachipana0102@gmail.com>
2022-07-01 16:38:26 -0500

    ADD: cardtransaction component

--- src/components/CardTransaction/card-transaction.js
+++ src/components/CardTransaction/card-transaction.js
@@ -1,5 +1,42 @@
-function CardTransaction() {
+import { categoryColors, categoryIcons } from "../../utils";
+import CircleIcon from "../CircleIcon";
+import * as Style from "./styles";
+
+function CardTransaction({ type, date, amount, transaction, description, tran_type, icon, color }) {
+  const newDate = new Date(date) // date should be a string: "2022-07-01"
+  const options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
+  const dateString = newDate.toLocaleDateString("en-US", options).replaceAll(",", "");
+  const [day, month, weekday, year] = dateString.split(" ");
   
+  return (
+    <Style.Container type={type}>
+      {
+        type === "day"
+        ?
+        <>
+          <Style.Day>{ weekday }</Style.Day>
+          <Style.Info>
+            <Style.Text isHead={false}>{ day }</Style.Text>
+            <Style.Text isHead={true}>{ `${month}, ${year}` }</Style.Text>
+          </Style.Info>
+          <Style.Amount tran_type={tran_type}>{ amount }</Style.Amount>
+        </>
+        :
+        <>
+          <CircleIcon 
+            color={categoryColors[color]}
+            Icon={categoryIcons[icon]}
+            inverted={true}
+          />
+          <Style.Info>
+            <Style.Text isHead={true}>{ transaction }</Style.Text>
+            <Style.Text isHead={false}>{ description || "No description" }</Style.Text>
+          </Style.Info>
+          <Style.Amount tran_type={tran_type}>{ amount }</Style.Amount>      
+        </>
+      }
+    </Style.Container>
+  )
 }
 
 export default CardTransaction;
