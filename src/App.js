077f69028f25bf9c3012b3b2526509a88662f145
emachipana <emachipana0102@gmail.com>
2022-07-01 12:23:52 -0500

    ADD: render test inputfilter

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import AuthenticatedApp from "./AuthenticatedApp";
+import InputFilter from "./components/InputFilter";
 import { useAuth } from "./context/auth-context";
 import UnauthenticatedApp from "./UnauthenticatedApp";
 
 function App() {
   const { user } = useAuth();
 
-  return user ? <AuthenticatedApp /> : <UnauthenticatedApp />;
+  //return user ? <AuthenticatedApp /> : <UnauthenticatedApp />;
+  return (
+    <form>
+      <InputFilter label="from" type="date" />
+      <InputFilter label="to" type="date" />
+    </form>
+  )
 }
 
 export default App;
