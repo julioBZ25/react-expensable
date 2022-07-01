cb3cc323cbc17faf0686eb2f296523c00bb3f371
emachipana <emachipana0102@gmail.com>
2022-07-01 12:43:00 -0500

    ADD: partial section of filter

--- src/components/Filter/filter.js
+++ src/components/Filter/filter.js
@@ -1,5 +1,16 @@
-function Filter() {
+import InputFilter from "../InputFilter";
+import * as Style from "./styles";
 
+function Filter() {
+  return (
+    <Style.Section>
+      <Style.Title>Date</Style.Title>
+      <Style.InputsContainer>
+        <InputFilter label="from" type="date" />
+        <InputFilter label="to" type="date" />
+      </Style.InputsContainer>
+    </Style.Section>
+  );
 }
 
 export default Filter;
