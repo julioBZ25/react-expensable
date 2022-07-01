a6dde39927d6aa67cdcc4769a14ab139df5bbdb8
emachipana <emachipana0102@gmail.com>
2022-07-01 17:55:02 -0500

    ADD: container

---
+++ src/components/Aside/ui.js
@@ -0,0 +1,28 @@
+import styled from "@emotion/styled";
+import { colors, fonts } from "../../styles";
+
+export const Section = styled.div`
+  max-width: 295px;
+  display: flex;
+  flex-direction: column;
+  gap: 0.5rem;
+  font-family: ${ fonts.primary };
+  color: ${colors.gray[500]};
+`;
+
+export const Title = styled.h2`
+  font-size: 16px;
+  line-height: 24px;
+`;
+
+export const InputsContainer = styled.div`
+  display: flex;
+  width: 100%;
+  gap: 1rem;
+`;
+
+export const CardsWrapper = styled.div`
+  max-width: 295px;
+  display: flex;
+  flex-direction: column;
+`;
