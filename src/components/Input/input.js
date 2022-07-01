85eb824bfed425c4dba4964b2e06fe63d0d194d1
emachipana <emachipana0102@gmail.com>
2022-07-01 12:09:43 -0500

    ADD: partial input structure

--- src/components/Input/input.js
+++ src/components/Input/input.js
@@ -1,5 +1,18 @@
-function Input() {
+import * as Style from "./styles";
 
+function Input({ label, type, value = "", placeholder = "" }) {
+  return (
+    <Style.Container>
+      <Style.Label htmlFor={label}>{label}</Style.Label>
+      <Style.Input
+        id={label}
+        name={label}
+        type={type || "text"}
+        value={value}
+        placeholder={placeholder}
+      />
+    </Style.Container>
+  );
 }
 
 export default Input;
--- src/components/Input/styles.js
+++ src/components/Input/styles.js
@@ -1,1 +1,28 @@
 import styled from "@emotion/styled";
+import { colors, fonts } from "../../styles";
+
+export const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  font-family: ${fonts.primary};
+  color: ${colors.gray[500]};
+  gap: 4px;
+`;
+
+export const Label = styled.label`
+  font-size: 12px;
+  line-height: 16px;
+  letter-spacing: 0.125em;
+  text-transform: uppercase;
+`;
+
+export const StyledInput = styled.input`
+  border: 1px solid ${colors.gray[200]};
+  outline: none;
+  box-sizing: border-box;
+  padding: 8px 12px;
+  gap: 10px;
+  background-color: ${ colors.white };
+  box-shadow: 0px 1px 2px rgba(0, 0, 0, 0.05);
+  border-radius: 6px;
+`;
