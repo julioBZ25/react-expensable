f63858f9c4a84472106f32ee673e55af7cdf0873
emachipana <emachipana0102@gmail.com>
2022-07-02 01:21:05 -0500

    FIX: card transaction

--- src/components/CardTransaction/card-transaction.js
+++ src/components/CardTransaction/card-transaction.js
@@ -3,11 +3,13 @@ import CircleIcon from "../CircleIcon";
 import * as Style from "./styles";
 
 function CardTransaction({ type, date, amount, transaction, description, tran_type, iconName, color }) {
-  const newDate = new Date(date) // date should be a string: "2022-07-01"
+  let parseDate = date.split("-");
+  const newDate = new Date(parseDate) // date should be a string: "2022-07-01"
   const options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
   const dateString = newDate.toLocaleDateString("en-US", options).replaceAll(",", "");
   const [day, month, weekday, year] = dateString.split(" ");
-  const symbol = tran_type === "expense" ? "-" : "+";
+  let symbol = amount < 0 ? "-" : "+";
+  symbol = tran_type === "expense" ? "-" : "+";
 
   return (
     <Style.Container type={type}>
@@ -15,24 +17,28 @@ function CardTransaction({ type, date, amount, transaction, description, tran_ty
         type === "day"
         ?
         <>
-          <Style.Day>{ weekday }</Style.Day>
-          <Style.Info>
-            <Style.Text isHead={false}>{ day }</Style.Text>
-            <Style.Text isHead={true}>{ `${month}, ${year}` }</Style.Text>
-          </Style.Info>
-          <Style.Amount tran_type={tran_type}>{symbol}${ amount }</Style.Amount>
+          <Style.Section>
+            <Style.Day>{ weekday }</Style.Day>
+            <Style.Info>
+              <Style.Text isHead={false}>{ day }</Style.Text>
+              <Style.Text isHead={true}>{ `${month}, ${year}` }</Style.Text>
+            </Style.Info>
+          </Style.Section>
+          <Style.Amount tran_type={symbol === "-" ? "expense" : "income"}>{symbol}${ amount }</Style.Amount>      
         </>
         :
         <>
-          <CircleIcon 
-            color={categoryColors[color]}
-            Icon={categoryIcons[iconName]}
-          />
-          <Style.Info>
-            <Style.Text isHead={true}>{ transaction }</Style.Text>
-            <Style.Text isHead={false}>{ description || "No description" }</Style.Text>
-          </Style.Info>
-          <Style.Amount tran_type={tran_type}>{symbol}${ amount }</Style.Amount>      
+          <Style.Section>
+            <CircleIcon 
+              color={categoryColors[color]}
+              Icon={categoryIcons[iconName]}
+            />
+            <Style.Info>
+              <Style.Text isHead={true}>{ transaction }</Style.Text>
+              <Style.Text isHead={false}>{ description || "No description" }</Style.Text>
+            </Style.Info>
+          </Style.Section>
+          <Style.Amount tran_type={symbol === "-" ? "expense" : "income"}>{symbol}${ amount }</Style.Amount>      
         </>
       }
     </Style.Container>
